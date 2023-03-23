0910fb3d1245ad6607dfe2883ab13a1dd53485ff
ArtemOsypov <artem.osypov@gmail.com>
2023-03-24 00:13:02 +0200

    #1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnClose = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', StartChangeColor);
+btnClose.addEventListener('click', StopChangeColor);
+
+function StartChangeColor() {
+  timerId = setInterval(changeColor, 1000);
+  btnStart.setAttribute('disabled', '');
+}
+
+function StopChangeColor() {
+  btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function changeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = `${color}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
