a3d7d9ed677dbf6add5f7e50be5c9050a97271e2
ArtemOsypov <artem.osypov@gmail.com>
2023-03-27 00:20:13 +0300

    task#2

---
+++ src/js/02-timer.js
@@ -0,0 +1,79 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+require('flatpickr/dist/themes/dark.css');
+
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  timerFieldDays: document.querySelector('[data-days]'),
+  timerFielHours: document.querySelector('[data-hours]'),
+  timerFieldMinutes: document.querySelector('[data-minutes]'),
+  timerFieldSeconds: document.querySelector('[data-seconds]'),
+};
+
+refs.btnStart.setAttribute('disabled', '');
+refs.btnStart.addEventListener('click', onBtnStartClick);
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  minDate: 'today',
+
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+      refs.btnStart.disabled = true;
+    } else {
+      refs.btnStart.disabled = false;
+    }
+  },
+};
+
+const fp = flatpickr('#datetime-picker', options);
+
+function onBtnStartClick() {
+  const selectedDate = fp.selectedDates[0];
+
+  timerId = setInterval(() => {
+    const startTime = new Date();
+    const countdown = selectedDate - startTime;
+    refs.btnStart.disabled = true;
+
+    if (countdown < 0) {
+      clearInterval(timerId);
+      return;
+    }
+    updateTimerFace(convertMs(countdown));
+  }, 1_000);
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  refs.timerFieldDays.textContent = addLeadingZero(days);
+  refs.timerFielHours.textContent = addLeadingZero(hours);
+  refs.timerFieldMinutes.textContent = addLeadingZero(minutes);
+  refs.timerFieldSeconds.textContent = addLeadingZero(seconds);
+}
